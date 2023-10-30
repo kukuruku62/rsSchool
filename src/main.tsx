3ca51f2864f74be7fd9b30e5e92c4f968a4b80e5
Aleksei Falchuk <sddw28@gmail.com>
2023-10-30 11:13:43 +0100

    feat: add searchdata function

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 import './index.css';
+import {SearchData} from "./utils/SearchData"
+
+const searchData = new SearchData();
+
+searchData.getAllItems().then(res => console.log(res.results))
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
